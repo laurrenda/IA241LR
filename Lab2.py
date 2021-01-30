216284285ff2839e1849a1ff1e3141dfbd5381f7
Lauryn Renda <rendalr@dukes.jmu.edu>
2021-01-30 21:14:09 +0000

    my Lab2

---
+++ Lab2.py
@@ -0,0 +1,44 @@
+#Lab2
+my_name = "Lauryn"
+
+#3.1
+
+print(my_name)
+
+#3.2
+
+my_id = 123
+print(my_id)
+
+#3.3
+
+#_123=my_id
+my_id =your_id=123
+print(my_id)
+print(your_id)
+
+#3.4
+my_id_str = '123'
+print(type(my_id_str))
+print(my_id_str)
+
+#3.5
+
+#print(my_name+my_id)
+
+#3.6
+
+print(my_name+my_id_str)
+
+#3.7
+
+print(my_name*3)
+
+#3.8 
+
+print('hello, world. This is my first python string.' .split('i'))
+
+#3.9
+
+message = "Lauryn's id is 123"
+print(message)
